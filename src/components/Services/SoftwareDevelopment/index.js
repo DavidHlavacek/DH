fcdd34103a7843ff0cae779f7b78817b085c0315
DavidHlavacek <davi.hlav@gmail.com>
2023-11-24 03:20:55 +0100

    gh-pages

--- src/components/Services/SoftwareDevelopment/index.js
+++ src/components/Services/SoftwareDevelopment/index.js
@@ -24,22 +24,22 @@ const Main = () => {
                     />
                 </h1>
                 <div className="paragraphs-container">
-                     <p>-Custom Software Development</p>
-                <p>-Mobile App Development</p>
-                <p>-Web Application Development</p>
-                <p>-Desktop Application Development</p>
-                <p>-Database Design and Management</p>
-                <p>-E-commerce Solutions</p>
-                <p>-Content Management Systems (CMS)</p>
-                <p>-Cloud-Based Solutions</p>
-                <p>-API Development and Integration</p>
-                <p>-Quality Assurance and Testing</p>
+                     <p>- Custom Software Development</p>
+                <p>- Mobile App Development</p>
+                <p>- Web Application Development</p>
+                <p>- Desktop Application Development</p>
+                <p>- Database Design and Management</p>
+                <p>- E-commerce Solutions</p>
+                <p>- Content Management Systems (CMS)</p>
+                <p>- Cloud-Based Solutions</p>
+                <p>- API Development and Integration</p>
+                <p>- Quality Assurance and Testing</p>
                 {/* <p>-DevOps Services (Unavailable for now)</p> */}
                 {/* <p>-Blockchain Development</p> */}
                 {/* <p>-Artificial Intelligence (AI) and Machine Learning (ML)</p> */}
-                <p>-Cybersecurity Solutions</p>
-                <p>-Maintenance and Support</p>
-                <p>-Consulting and Advisory Services</p>
+                <p>- Cybersecurity Solutions</p>
+                <p>- Maintenance and Support</p>
+                <p>- Consulting and Advisory Services</p>
                 {/* <p>-Training and Workshops</p> */}
                 </div>
             </div>
